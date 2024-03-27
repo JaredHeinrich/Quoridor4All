8fd2987bb047e7098943455beedc3950c6800c23
Valentin <v.spam@cze-kalla.de>
2024-03-27 14:06:21 +0100

    completed dependency installation

---
+++ Frontend/quoridor4all/src/routes/start/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Input, Label, Button } from 'flowbite-svelte';
+</script>
+
+<form>
+  <div class="grid gap-6 mb-6 md:grid-cols-2">
+    <div>
+      <Label for="first_name" class="mb-2">First name</Label>
+      <Input type="text" id="first_name" placeholder="John" required />
+    </div>
+    <div>
+      <Button type="submit">Submit</Button>
+    </div>
+  </div>
+</form>
