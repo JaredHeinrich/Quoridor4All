2f1f5c36a3af1bc2dae1281c4716e88cdb48897a
Valentin <v.spam@cze-kalla.de>
2024-03-28 09:16:01 +0100

    initial start page design and routing to game

--- Frontend/quoridor4all/src/routes/game/+page.svelte
+++ Frontend/quoridor4all/src/routes/game/+page.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
-  import {Input} from 'flowbite-svelte';
+  import { onMount } from "svelte";
+  import { players as playersStore } from "../../store";
+  
+  let players: any = null;
+
+
+
+  onMount(() => {
+    //fetch data from backend, players should be created in backend
+
+    players = $playersStore.map((player) => {
+      return {
+        xPosition: null, 
+        yPosition: null,
+        number_of_available_walls: 6, 
+        goal: null, 
+        player_name: player,
+      };
+    });
+
+    console.log(players);
+  });
 </script>
 
 <div>
-  <form>
-    <Input />
-    <Input />
-  </form>
+  <h1>Heading</h1>
 </div>
---
+++ Frontend/quoridor4all/src/routes/start/PlayerInput.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Input, Label, Button } from 'flowbite-svelte';
+
+  export let playerNumber: number;
+  export let playerName: string;
+</script>
+    
+<div>
+  <Label for="player_name" class="mb-2">Spieler {playerNumber}</Label>
+  <Input bind:value={playerName} type="text" id="player_name" placeholder="Spieler {playerNumber}" required />
+</div>
