c03600c280dd7c46483b338578389ce542f85333
Valentin <v.spam@cze-kalla.de>
2024-04-19 18:22:39 +0200

    first version of path checker

--- quoridor4all/src-tauri/src/structs/game.rs
+++ quoridor4all/src-tauri/src/structs/game.rs
@@ -3,9 +3,9 @@ use std::i16;
 use serde::{Deserialize, Serialize};
 
 use super::history::{Move, PawnMove};
-use super::{pawn::Pawn, history::GameHistory, wall::Wall};
-use crate::enums::{Color, Side, Direction};
-use crate::vector_util::{VectorUtil, Vector};
+use super::{history::GameHistory, pawn::Pawn, wall::Wall};
+use crate::enums::{Color, Direction, Side};
+use crate::vector_util::{Vector, VectorUtil};
 use crate::NUMBER_OF_PLAYERS;
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -21,24 +21,31 @@ pub struct Game {
     current_pawn: CurrentPawn,
     history: GameHistory,
     board_size: i16,
-    walls: Vec<Wall>
+    walls: Vec<Wall>,
 }
 
 impl Game {
-    pub fn new(board_size: i16, number_of_walls_per_player: i16, players: [Player;NUMBER_OF_PLAYERS]) -> Self {
-        let pawns: Vec<Pawn> = players.into_iter()
-            .map(|player| Pawn::new(
+    pub fn new(
+        board_size: i16,
+        number_of_walls_per_player: i16,
+        players: [Player; NUMBER_OF_PLAYERS],
+    ) -> Self {
+        let pawns: Vec<Pawn> = players
+            .into_iter()
+            .map(|player| {
+                Pawn::new(
                     board_size,
                     player.pawn_side,
                     number_of_walls_per_player,
                     player.player_name,
                     player.pawn_color,
-                    ))
+                )
+            })
             .collect();
-        let current_pawn: CurrentPawn = CurrentPawn(0); 
+        let current_pawn: CurrentPawn = CurrentPawn(0);
         let history: GameHistory = GameHistory::new();
         let walls: Vec<Wall> = Vec::new();
-        Self{
+        Self {
             pawns,
             current_pawn,
             history,
@@ -50,7 +57,11 @@ impl Game {
     pub fn board_size(&self) -> i16 {
         self.board_size
     }
-    pub fn move_current_pawn(&mut self, new_position: Vector, allowed_positions: &Vec<Vector>) -> Result<Vector,String> {
+    pub fn move_current_pawn(
+        &mut self,
+        new_position: Vector,
+        allowed_positions: &Vec<Vector>,
+    ) -> Result<Vector, String> {
         if !allowed_positions.contains(&new_position) {
             return Err("not a valid move".to_string());
         }
@@ -59,7 +70,8 @@ impl Game {
         pawn.move_pawn(movement);
         let new_pos = pawn.position();
         self.current_pawn.set_next();
-        self.history.add_move(Move::PawnMove(PawnMove::new(movement)));
+        self.history
+            .add_move(Move::PawnMove(PawnMove::new(movement)));
         Ok(new_pos)
     }
 
@@ -73,7 +85,12 @@ impl Game {
         res
     }
 
-    fn check_step(&self, move_direction: &Direction, pawn_position: Vector, jumps: i16) -> Vec<Vector> {
+    fn check_step(
+        &self,
+        move_direction: &Direction,
+        pawn_position: Vector,
+        jumps: i16,
+    ) -> Vec<Vector> {
         let mut res = Vec::new();
         if jumps > NUMBER_OF_PLAYERS as i16 - 1 {
             return res;
@@ -92,14 +109,13 @@ impl Game {
                 new_pos_on_pawn = true;
                 break;
             }
-        };
+        }
         if new_pos_on_pawn {
-            return self.check_jump(move_direction, new_pos, jumps+1);
+            return self.check_jump(move_direction, new_pos, jumps + 1);
         }
         res.push(new_pos);
         res
     }
-
 
     fn check_jump(&self, move_direction: &Direction, pawn_pos: Vector, jumps: i16) -> Vec<Vector> {
         let mut res = Vec::new();
@@ -111,7 +127,6 @@ impl Game {
         res
     }
 
-
     pub fn is_wall_valid(&self, new_wall: &Wall) -> bool {
         let new_wall_pos = new_wall.position();
         if !new_wall_pos.is_on_wall_grid(self) {
@@ -121,7 +136,7 @@ impl Game {
             if new_wall.is_in_conflict_with(wall) {
                 return false;
             }
-        };
+        }
         let new_wall = new_wall.clone();
         let mut temp_game = self.clone();
         temp_game.walls.push(new_wall);
@@ -131,11 +146,14 @@ impl Game {
         true
     }
 
-    pub fn place_wall(&mut self, new_wall: Wall) -> Result<(),String>{
+    pub fn place_wall(&mut self, new_wall: Wall) -> Result<(), String> {
         if !self.is_wall_valid(&new_wall) {
             return Err("wall is not valid".to_string());
         }
-        self.pawns.get_mut(self.current_pawn.get()).unwrap().dec_number_of_walls();
+        self.pawns
+            .get_mut(self.current_pawn.get())
+            .unwrap()
+            .dec_number_of_walls();
         self.walls.push(new_wall);
         self.current_pawn.set_next();
         Ok(())
@@ -150,25 +168,82 @@ impl Game {
         true
     }
 
-    pub fn check_pawn_path(&self, pawn_index:usize) -> bool {
-        true
+    pub fn check_pawn_path(&self, pawn_index: usize) -> bool {
+        let mut visited_positions = Vec::new();
+        let mut current_position = self.pawns[pawn_index].position();
+
+        //first position is already visited
+        visited_positions.push(current_position.clone());
+        let mut index: usize = 0;   //index to go over all visited_positions and add possible possitions for all positions in visited_positions
+
+        //if for all visited_positions all possible neighbor positions are checked
+        while visited_positions.get(index).is_some() {
+            current_position = visited_positions[index].clone();
+
+            //current position is a goal -->path found
+            if self.pawns[pawn_index].goal().is_in_goal_line(&current_position) {
+                return true;
+            }
+
+            //get neighbour positions which are valid
+            let valid_positions = self.get_valid_next_positions_without_other(&current_position, pawn_index);
+
+            //add valid_positions to visited_positions if not already in visited_positions
+            for position in valid_positions {
+                if !visited_positions.contains(&position) {
+                    visited_positions.push(position);
+                }
+            }
+
+            //next position in visited_positions
+            index += 1;
+        }
+        false
     }
 
-    fn does_wall_block_move(&self, move_direction: &Direction, pawn_pos: Vector) -> bool{
+    //get_valid_neighbor_positions
+    fn get_valid_next_positions_without_other(&self, pawn_index: usize) -> Vec<Vector> {
+        let mut res: Vec<Vector> = Vec::new();
+        let pawn_pos = self.pawns.get(pawn_index).unwrap().position();
+
+        res.push(&mut self.check_step_without_other(&Direction::Up, pawn_pos));
+        res.push(&mut self.check_step_without_other(&Direction::Right, pawn_pos));
+        res.push(&mut self.check_step_without_other(&Direction::Down, pawn_pos));
+        res.push(&mut self.check_step_without_other(&Direction::Left, pawn_pos));
+        res
+    }
+
+    fn check_step_without_other(
+        &self,
+        move_direction: &Direction,
+        pawn_position: Vector,
+    ) -> Vector {
+        let movement = move_direction.to_vector();
+        let new_pos = pawn_position.add(movement);
+        if !new_pos.is_on_pawn_grid(self) {
+            return;
+        };
+        if self.does_wall_block_move(&move_direction, pawn_position) {
+            return;
+        }
+        new_pos
+    }
+
+    fn does_wall_block_move(&self, move_direction: &Direction, pawn_pos: Vector) -> bool {
         let mut blocking_walls: Vec<Wall> = Vec::new();
         match move_direction {
             Direction::Up => {
-                let pos_a = pawn_pos.add(Vector::new(-1,-1));
-                let pos_b = pawn_pos.add(Vector::new(0,-1));
+                let pos_a = pawn_pos.add(Vector::new(-1, -1));
+                let pos_b = pawn_pos.add(Vector::new(0, -1));
                 if pos_a.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_a, true))
                 }
                 if pos_b.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_b, true))
                 }
-            },
+            }
             Direction::Right => {
-                let pos_a = pawn_pos.add(Vector::new(0,-1));
+                let pos_a = pawn_pos.add(Vector::new(0, -1));
                 let pos_b = pawn_pos;
                 if pos_a.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_a, false))
@@ -176,9 +251,9 @@ impl Game {
                 if pos_b.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_b, false))
                 }
-            },
+            }
             Direction::Down => {
-                let pos_a = pawn_pos.add(Vector::new(-1,0));
+                let pos_a = pawn_pos.add(Vector::new(-1, 0));
                 let pos_b = pawn_pos;
                 if pos_a.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_a, true))
@@ -186,17 +261,17 @@ impl Game {
                 if pos_b.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_b, true))
                 }
-            },
+            }
             Direction::Left => {
-                let pos_a = pawn_pos.add(Vector::new(-1,-1));
-                let pos_b = pawn_pos.add(Vector::new(-1,0));
+                let pos_a = pawn_pos.add(Vector::new(-1, -1));
+                let pos_b = pawn_pos.add(Vector::new(-1, 0));
                 if pos_a.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_a, false))
                 }
                 if pos_b.is_on_wall_grid(self) {
                     blocking_walls.push(Wall::new(pos_b, false))
                 }
-            },
+            }
         }
         for wall in self.walls.iter() {
             if blocking_walls.contains(&wall) {
@@ -205,20 +280,18 @@ impl Game {
         }
         false
     }
-}// impl Game
+} // impl Game
 
 #[derive(Clone)]
 struct CurrentPawn(usize);
-impl CurrentPawn{
+impl CurrentPawn {
     fn get(&self) -> usize {
         self.0
     }
     fn set_next(&mut self) {
-        self.0 = (self.0 + 1)%NUMBER_OF_PLAYERS;
+        self.0 = (self.0 + 1) % NUMBER_OF_PLAYERS;
     }
     fn set_prev(&mut self) {
-        self.0 = (self.0 + NUMBER_OF_PLAYERS - 1)%NUMBER_OF_PLAYERS;
+        self.0 = (self.0 + NUMBER_OF_PLAYERS - 1) % NUMBER_OF_PLAYERS;
     }
 }
-
-
--- quoridor4all/src-tauri/src/structs/goal.rs
+++ quoridor4all/src-tauri/src/structs/goal.rs
@@ -43,4 +43,12 @@ impl Goal {
         self.goal_line
     }
     //getter
+    
+    pub fn is_in_goal_line(&self, position: Vector) -> bool {
+        match self.is_x_coordinate {
+            true => position.x() == self.goal_line(),
+            false => position.y() == self.goal_line(),
+        }
+    }
+    
 }
